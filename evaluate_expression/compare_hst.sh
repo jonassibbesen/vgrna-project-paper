e47949c1d0df376230ef93e766dd2c8f64d550ed
jonassibbesen <j.a.sibbesen@gmail.com>
2021-03-11 15:33:00 -0800

    add evaluate expression scripts

---
+++ evaluate_expression/compare_hst.sh
@@ -0,0 +1,16 @@
+set -e
+
+# Set simulated transcript prefix
+SIM_PREFIX="1kg_NA12878_exons_gencode100_allpaths"
+
+# Set transcript prefix
+TRANSCRIPTS_PREFIX="1kg_nonCEU_af001_gencode100"
+
+# Set output name prefix
+OUT_PREFIX="1kg_NA12878_exons_gencode100_allpaths"
+
+# Set number of threads
+CPU=1
+
+# Compare hst sequences
+/usr/bin/time -v bash -c "python3 /scripts/vgrna/compare_hst_sequences.py <(zcat ${SIM_PREFIX}.fa.gz) <(zcat ${TRANSCRIPTS_PREFIX}.fa.gz) ${OUT_PREFIX}.txt; wc -l ${OUT_PREFIX}.txt"
